9ef1150da9cd4f4fb978426105434bad06379e64
이혜연 <dlgpdus0416@naver.com>
2022-05-20 00:59:53 +0900

    PUB_SUB server code 구현 완료

---
+++ src/main/java/PUB_SUB/lec_05_prg_03_pub_sub_basic_server.java
@@ -0,0 +1,25 @@
+package PUB_SUB;
+
+import org.zeromq.SocketType;
+import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
+import java.util.Random;
+
+public class lec_05_prg_03_pub_sub_basic_server {
+    public static void main(String args[]){
+        System.out.println("Publishing updates at weather server...");
+
+        ZContext context = new ZContext();
+        ZMQ.Socket socket = context.createSocket(SocketType.PUB);
+        socket.bind("tcp://localhost:5555");
+
+        while(true){
+            int zipcode = (int)(Math.random()*99998+1);
+            int temperature = (int)(Math.random()*214-80);
+            int relhumidity = (int)(Math.random()*49+10);
+
+            String response = Integer.toString(zipcode) +" "+ Integer.toString(temperature) +" "+ Integer.toString(relhumidity);
+            System.out.println(response);
+        }
+    }
+}
---
+++ src/main/java/PUB_SUB/lec_05_prg_04_pub_sub_basic_client.java
@@ -0,0 +1,11 @@
+package PUB_SUB;
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+
+public class lec_05_prg_04_pub_sub_basic_client {
+    public static void main(String Args[]){
+        ZContext context = new ZContext();
+        //ZMQ.Socket socket = context.createSocket();
+    }
+}
