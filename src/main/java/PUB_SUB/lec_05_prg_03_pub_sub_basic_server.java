5f78786851c99cf5efe1bf9534a35f11e1ed37ba
이혜연 <dlgpdus0416@naver.com>
2022-05-22 01:11:49 +0900

    주석 처리 및 불필요한 디버깅용 코드 제거

--- src/main/java/PUB_SUB/lec_05_prg_03_pub_sub_basic_server.java
+++ src/main/java/PUB_SUB/lec_05_prg_03_pub_sub_basic_server.java
@@ -14,14 +14,13 @@ public class lec_05_prg_03_pub_sub_basic_server {
         socket.bind("tcp://localhost:5555");
 
         while(true){
-            int zipcode = (int)(Math.random()*99998+1);
-            int temperature = (int)(Math.random()*214-80);
-            int relhumidity = (int)(Math.random()*49+10);
+            int zipcode = (int)(Math.random()*99998+1); //1~99999
+            int temperature = (int)(Math.random()*214-80); //-80~134
+            int relhumidity = (int)(Math.random()*49+10); //10~59
 
+            //integer을 string으로 변환하여 합침
             String response = Integer.toString(zipcode) +" "+ Integer.toString(temperature) +" "+ Integer.toString(relhumidity);
             socket.send(response);
-
-            System.out.println(response);
         }
     }
 }
--- src/main/java/PUB_SUB/lec_05_prg_04_pub_sub_basic_client.java
+++ src/main/java/PUB_SUB/lec_05_prg_04_pub_sub_basic_client.java
@@ -9,16 +9,18 @@ public class lec_05_prg_04_pub_sub_basic_client {
         ZMQ.Socket socket = context.createSocket(SocketType.SUB);
 
         System.out.println("Collecting updates from seather server...");
-
         socket.connect("tcp://localhost:5555");
 
         String zip_filter;
 
         if (Args.length>0){
+            //접두어를 기준으로 필터링 하므로, 다섯자리 미만의 숫자도 제대로 받아오도록 공백을 붙임
+            // ex) 50을 입력했을 때 505, 50232 등을 필터링 하지 않도록 함
             zip_filter = Args[0] + " ";
         }else{
             zip_filter = "10001";
         }
+        //subscribe socket에 필터를 거는 부분
         socket.subscribe(zip_filter.getBytes());
 
         int total_temp = 0;
