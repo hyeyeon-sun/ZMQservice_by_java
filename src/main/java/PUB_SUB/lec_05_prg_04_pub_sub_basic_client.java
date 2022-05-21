8ccbf38747f1ecc4211d5cce91771e536a0a3b3b
이혜연 <dlgpdus0416@naver.com>
2022-05-21 23:40:27 +0900

    filter 기능 제외 PUB-SUB 클라이언트 코드 구현

--- src/main/java/PUB_SUB/lec_05_prg_04_pub_sub_basic_client.java
+++ src/main/java/PUB_SUB/lec_05_prg_04_pub_sub_basic_client.java
@@ -6,6 +6,27 @@ import org.zeromq.ZContext;
 public class lec_05_prg_04_pub_sub_basic_client {
     public static void main(String Args[]){
         ZContext context = new ZContext();
-        //ZMQ.Socket socket = context.createSocket();
+        ZMQ.Socket socket = context.createSocket(SocketType.SUB);
+
+        System.out.println("Collecting updates from seather server...");
+        socket.connect("tcp://localhost:5555");
+
+        //system argument//
+
+        String zip_filter = "10001";
+
+        //set socket opt//
+
+        int total_temp = 0;
+        for (int update_nbr = 0; update_nbr<20; update_nbr++){
+            String string = socket.recvStr();
+            String[] info = string.split(" ");
+            String zipcode = info[0];
+            String temperature = info[1];
+            String relhumidity = info[2];
+            total_temp += Integer.parseInt(temperature);
+
+            System.out.println("Receive temperature for zipcode" + zip_filter + "was" + temperature);
+        }
     }
 }
