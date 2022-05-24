595421a85c3f3682cc6556a342bcae0a9fa68693
이혜연 <dlgpdus0416@naver.com>
2022-05-24 16:10:45 +0900

    pub_sub and push_pull v2 코드 초안 작성

--- src/main/java/PUB_SUB_PUSH_PULL/lec_05_prg_05_pub_sub_and_pull_push_client_v2.java
+++ src/main/java/PUB_SUB_PUSH_PULL/lec_05_prg_05_pub_sub_and_pull_push_client_v2.java
@@ -1,11 +1,11 @@
 package PUB_SUB_PUSH_PULL;
 
 import org.zeromq.SocketType;
-import org.zeromq.ZMQ;
 import org.zeromq.ZContext;
+import org.zeromq.ZMQ;
 
-public class lec_05_prg_06_pub_sub_and_pull_push_client {
-    public static void main (String args[]){
+public class lec_05_prg_05_pub_sub_and_pull_push_client_v2 {
+    public static void main (String args[]) {
         ZContext context = new ZContext();
         ZMQ.Socket subscriber = context.createSocket(SocketType.SUB);
         subscriber.subscribe("".getBytes());
@@ -13,30 +13,24 @@ public class lec_05_prg_06_pub_sub_and_pull_push_client {
         ZMQ.Socket publisher = context.createSocket(SocketType.PUSH);
         publisher.connect("tcp://localhost:5558");
 
-        //poller
-        //&& subscriber.setReceiveTimeOut(10000)
-        int condition = subscriber.getEvents() & ZMQ.Poller.POLLIN;
-
-        while(true){
-            //System.out.println(subscriber.getEvents());
-            if(subscriber.getEvents() == 1){
+        String clientID = args[1];
+        while (true) {
+            if (subscriber.getEvents() == 1) {
                 try {
                     Thread.sleep(1000);
-                } catch(Exception e){
+                } catch (Exception e) {
 
                 }
                 byte[] message = subscriber.recv();
                 String s_message = new String(message);
                 System.out.println("I: received message " + s_message);
-            }else{
-                int rand = (int)(Math.random()*9999+1);
-                if(rand < 10){
+            } else {
+                int rand = (int) (Math.random() * 9999 + 1);
+                if (rand < 10) {
                     publisher.send(Integer.toString(rand));
                     System.out.println("I: sending message" + Integer.toString(rand));
                 }
             }
         }
-
-
     }
 }
