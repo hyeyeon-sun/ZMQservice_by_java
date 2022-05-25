a4c44c38d92782ce79c21e04384d6383b8c5bf99
이혜연 <dlgpdus0416@naver.com>
2022-05-25 17:44:34 +0900

    pub_sub_push_pull_v2 client 코드 구현

--- src/main/java/PUB_SUB_PUSH_PULL/lec_05_prg_05_pub_sub_and_pull_push_client_v2.java
+++ src/main/java/PUB_SUB_PUSH_PULL/lec_05_prg_05_pub_sub_and_pull_push_client_v2.java
@@ -16,19 +16,31 @@ public class lec_05_prg_05_pub_sub_and_pull_push_client_v2 {
         String clientID = args[1];
         while (true) {
             if (subscriber.getEvents() == 1) {
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-
-                }
                 byte[] message = subscriber.recv();
                 String s_message = new String(message);
-                System.out.println("I: received message " + s_message);
+                System.out.println(clientID + ": receive status => " + s_message);
             } else {
-                int rand = (int) (Math.random() * 9999 + 1);
+                int rand = (int) (Math.random() * 99 + 1);
                 if (rand < 10) {
-                    publisher.send(Integer.toString(rand));
-                    System.out.println("I: sending message" + Integer.toString(rand));
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+
+                    }
+                    String msg = "(" + clientID + ":ON)";
+                    publisher.send(msg);
+                    System.out.println(clientID + ": send status = activated");
+                }
+                else if (rand > 90){
+                    /*
+                    try {
+                        Thread.sleep(1000);
+                    } catch (Exception e) {
+
+                    }*/
+                    String msg = "(" + clientID + ":OFF)";
+                    publisher.send(msg);
+                    System.out.println(clientID + ": send status - deactivated");
                 }
             }
         }
